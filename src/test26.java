2acf8628d68f9937b8d195b60d30d726e5a6098d
jeong-hyun-woo <jhw504166@gmail.com>
2022-05-02 00:43:11 +0900

    javatest

---
+++ src/test26.java
@@ -0,0 +1,12 @@
+public class test26 {
+    public static void main(String[] args) {
+        //배열
+
+        String [] classGroup = {"정현우", "이재윤", "박상욱", "노윤재"};
+        System.out.println(classGroup[0]);
+        System.out.println(classGroup[1]);
+        System.out.println(classGroup[2]);
+        System.out.println(classGroup[3]);
+
+    }
+}
---
+++ src/test27.java
@@ -0,0 +1,14 @@
+public class test27 {
+    public static void main(String[] args) {
+        //배열2
+        String[] classgroup = new String[4];
+        classgroup[0] = "정현우";
+        System.out.println(classgroup.length);
+        classgroup[1] = "이재윤";
+        System.out.println(classgroup.length);
+        classgroup[2] = "박상욱";
+        System.out.println(classgroup.length);
+        classgroup[3] = "노윤재";
+        System.out.println(classgroup.length);
+    }
+}
