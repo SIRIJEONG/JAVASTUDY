be6923c9796f38729cb38ace07097eaab0f3d0ed
jeong-hyun-woo <jhw504166@gmail.com>
2022-05-02 23:25:42 +0900

    javatest

---
+++ src/test28.java
@@ -0,0 +1,12 @@
+public class test28 {
+    public static void main(String[] args) {
+        //배열 반복문
+        String[] members = {"정현우", "이재윤", "박상욱"};
+
+        for (int i = 0; i< members.length; i++) {
+            String member = members[i];
+            System.out.println(member + "이 상담을 받았습니다.");
+        }
+
+    }
+}
---
+++ src/test29.java
@@ -0,0 +1,11 @@
+public class test29 {
+    public static void main(String[] args) {
+        //for each
+
+        String[] members = {"정현우", "이재윤", "박상욱"};
+
+        for (String e : members) {
+            System.out.println(e + "이 상담을 받았습니다.");
+        }
+    }
+}
